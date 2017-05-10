b0a144b6de3f20da696f25a91ddf7ced85bfc7e0
Samir Gossain <samir.gossain@gmail.com>
2017-05-10 18:31:55 +0100

    First Price Test Failing

---
+++ lib/price.rb
@@ -0,0 +1,10 @@
+class Price
+
+  def initialize
+    @price = 0
+  end
+
+  def price(order)
+  end
+
+end
---
+++ spec/price_spec.rb
@@ -0,0 +1,21 @@
+require 'price'
+require 'order'
+
+describe Price do
+
+  # before do
+  #   order = Order.new
+  #   order.clock_number('WNP/SWCL001/010')
+  #   order.standard_delivery_companies('Disney', 'Discovery', 'Viacom')
+  #   order.express_delivery_companies('Horse & County')
+  # end
+
+  it 'Calculates the Price of an Order' do
+    order = Order.new
+    order.clock_number('WNP/SWCL001/010')
+    order.standard_delivery_companies('Disney', 'Discovery', 'Viacom')
+    order.express_delivery_companies('Horse & County')
+    expect(price(order)).to eq 50
+  end
+
+end
