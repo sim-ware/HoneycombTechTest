c9d7f7736d1bb308dd47043158b3712fe674a4e4
Samir Gossain <samir.gossain@gmail.com>
2017-05-11 23:00:33 +0100

    Order Class Tested

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,29 +2,44 @@ require 'order'
 
 describe Order do
 
-  it 'initialises with empty attributes' do
-    order = Order.new
-    expect(order.clock).to eq ''
-    expect(order.standard_del).to eq []
-    expect(order.express_del).to eq []
+  describe "::new" do
+    subject { Order.new }
+
+    it "creates a new Order" do
+      expect(subject).to be_instance_of(Order)
+    end
+
+    it "has a default empty attributes" do
+      expect(subject.clock).to eq('')
+      expect(subject.standard_del).to eq([])
+      expect(subject.express_del).to eq([])
+      expect(subject.price).to eq(0)
+      expect(subject.discount_price).to eq(0)
+    end
   end
 
-  it 'stores a string for the "Clock" number' do
-    order = Order.new
-    order.clock_number('WNP/SWCL001/010')
-    expect(order.clock).to eq 'WNP/SWCL001/010'
+  describe '#clock_number'do
+    it 'stores a string for the "Clock" number' do
+      order = Order.new
+      order.clock_number('WNP/SWCL001/010')
+      expect(order.clock).to eq 'WNP/SWCL001/010'
+    end
   end
 
-  it 'stores an Array for "standard_del" Companies' do
-    order = Order.new
-    order.standard_delivery_companies('Disney', 'Discovery', 'Viacom')
-    expect(order.standard_del).to eq ['Disney', 'Discovery', 'Viacom']
+  describe '#standard_delivery_companies' do
+    it 'stores an Array for "standard_del" Companies' do
+      order = Order.new
+      order.standard_delivery_companies('Disney', 'Discovery', 'Viacom')
+      expect(order.standard_del).to eq ['Disney', 'Discovery', 'Viacom']
+    end
   end
 
-  it 'stores a Array for "express_del" Companies' do
-    order = Order.new
-    order.express_delivery_companies('Horse & County')
-    expect(order.express_del).to eq ['Horse & County']
+  describe '#express_delivery_companies' do
+    it 'stores a Array for "express_del" Companies' do
+      order = Order.new
+      order.express_delivery_companies('Horse & County')
+      expect(order.express_del).to eq ['Horse & County']
+    end
   end
 
 end
