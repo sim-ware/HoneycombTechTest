325415729e2fc3a88cdded75cd9f5367a56941d6
Samir Gossain <samir.gossain@gmail.com>
2017-05-11 23:05:20 +0100

    Refactors Order Class Tests

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -20,25 +20,25 @@ describe Order do
 
   describe '#clock_number'do
     it 'stores a string for the "Clock" number' do
-      order = Order.new
-      order.clock_number('WNP/SWCL001/010')
-      expect(order.clock).to eq 'WNP/SWCL001/010'
+      subject = Order.new
+      subject.clock_number('WNP/SWCL001/010')
+      expect(subject.clock).to eq 'WNP/SWCL001/010'
     end
   end
 
   describe '#standard_delivery_companies' do
     it 'stores an Array for "standard_del" Companies' do
-      order = Order.new
-      order.standard_delivery_companies('Disney', 'Discovery', 'Viacom')
-      expect(order.standard_del).to eq ['Disney', 'Discovery', 'Viacom']
+      subject = Order.new
+      subject.standard_delivery_companies('Disney', 'Discovery', 'Viacom')
+      expect(subject.standard_del).to eq ['Disney', 'Discovery', 'Viacom']
     end
   end
 
   describe '#express_delivery_companies' do
     it 'stores a Array for "express_del" Companies' do
-      order = Order.new
-      order.express_delivery_companies('Horse & County')
-      expect(order.express_del).to eq ['Horse & County']
+      subject = Order.new
+      subject.express_delivery_companies('Horse & County')
+      expect(subject.express_del).to eq ['Horse & County']
     end
   end
 
--- spec/pricer_spec.rb
+++ spec/pricer_spec.rb
@@ -3,13 +3,23 @@ require 'order'
 
 describe Pricer do
 
-  it 'calculates the Price of an Order' do
-    order = Order.new
-    p = Pricer.new
-    order.standard_delivery_companies('Disney', 'Discovery', 'Viacom')
-    order.express_delivery_companies('Horse & County')
-    p.pricing(order)
-    expect(order.price).to eq 50
+  describe "::new" do
+    subject { Pricer.new }
+
+    it "creates a new Pricer" do
+      expect(subject).to be_instance_of(Pricer)
+    end
+  end
+
+  describe '#pricing' do
+    it 'calculates the Price of an Order' do
+      order = Order.new
+      p = Pricer.new
+      order.standard_delivery_companies('Disney', 'Discovery', 'Viacom')
+      order.express_delivery_companies('Horse & County')
+      p.pricing(order)
+      expect(order.price).to eq 50
+    end
   end
 
 end
